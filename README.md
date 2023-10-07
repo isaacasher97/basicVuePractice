316fc9d5b1035934553dc048942bdbdbd3678930
isaacasher97 <isaac.asher97@gmail.com>
2023-10-07 05:14:31 -0400

    initial commit for deployment-styling needed

---
+++ README.md
@@ -0,0 +1,37 @@
+# My Latest Vue Practice: Basics
+
+- **Contributors:** Isaac Asher
+- [**Deployed Site**]()
+
+## 🧑‍💻 Technologies 🧑‍💻
+
+- Vue 
+- HTML
+- CSS
+- JAVASCRIPT
+
+## 🗒️ Description 🗒️
+
+This is just a simple Front End Practice Page Built With Vue! You will see certain basics displayed on the page. Please feel free to clone down this code and make a pull request if you'd like to contribute!
+
+## 🔍 Main Features 🔍
+Add Your diagnosis to keep record of any past, current, or future symptoms
+
+- Elements Using v-else, v-if, v-on (also called @click) events
+- v-for used for outputting lists/arrays
+- Mouseover Events (Open the console to view these events in real time)
+
+## ERD (ENTITY RELATIONSHIP DIAGRAM) 🩺
+``` mermaid
+erDiagram
+    
+    DATA {
+        str url
+        bool showData
+        int x
+        int y
+        arr students
+        bool graduated
+        arr books
+    }
+```
---
+++ app.js
@@ -0,0 +1,42 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            url: 'https://isaacasher.dev',
+            showData: true,
+            x: 0,
+            y: 0,
+            students: [
+                { studentId: 1, name: 'Isaac', age: 26, currentSchool: 'General Assembly' },
+                { studentId: 2, name: 'John', age: 27, currentSchool: 'General Assembly' },
+                { studentId: 3, name: 'David', age: 28, currentSchool: 'General Assembly' }
+            ],
+            graduated: true,
+            books: [
+                { title: 'Harry Potter 1', author: 'J.K.Rowling', img: './assets/hp1.jpg' },
+                { title: 'Harry Potter 2', author: 'J.K.Rowling', img: './assets/hp2.jpg' },
+                { title: 'Harry Potter 3', author: 'J.K.Rowling', img: './assets/hp3.jpg' }
+            ]
+        }
+    },
+    methods: {
+        toggleShowData() {
+            this.showData = !this.showData
+        },
+        isGraduated() {
+            this.graduated = !this.graduated
+        },
+        handleEvent(e, data) {
+            console.log(e, e.type)
+            if(data) {
+                console.log(data)
+            }
+        },
+        handleMouseMove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
+            console.log(e)
+        }
+    }
+})
+
+app.mount('#app')
